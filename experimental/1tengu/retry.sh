3af95f1157f59c589f2267495b6d898e3d86d314
7shi <7shi@live.jp>
2025-06-29 16:52:32 +0900

    tengu評価スクリプトで構造化出力の温度設定オプションに変更

--- experimental/1tengu/retry.sh
+++ experimental/1tengu/retry.sh
@@ -3,6 +3,6 @@
 # 1042/119=8.76 o4-mini/gemini-2.5-pro-preview-03-25
 # 1004/119=8.44 o4-mini/gemini-2.5-flash
 
-time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-flash.json -m o4-mini --disable-temperature --all
-time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-pro-preview-03-25.json -m o4-mini --disable-temperature --all
-time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-pro-preview-05-06.json -m o4-mini --disable-temperature --all
+time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-flash.json -m o4-mini -st -1 --all
+time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-pro-preview-03-25.json -m o4-mini -st -1 --all
+time uv run tengu.py ../../data/model_answers/lightblue__tengu_bench/gemini-2.5-pro-preview-05-06.json -m o4-mini -st -1 --all
